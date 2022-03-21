6e6531a977ad49e5f1f153640fa3b16ec3cf6a52
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2022-03-21 00:08:27 -0300

    Instalação do Dotenv para configuração das variáveis de ambiente do sistema

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import clientsRoutes from "./routes/clientsRoutes";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(clientsRoutes);
 app.set('view engine', 'pug');
