40f33bc50afdfc659cf930d94a8cf5176dd79a44
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2022-03-20 23:21:08 -0300

    Criação da pasta routes e configuração inicial do arquivo de rotas clientsRoutes

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import clientsRoutes from "./routes/clientsRoutes";
 
 const app = express();
 const port = 3000;
+
+app.use(clientsRoutes);
 
 app.listen(port, () => {
     console.log(`Servidor iniciado com sucesso na porta ${port}`);
